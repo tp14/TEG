c37b82f38813cd4eb5c1a140eb3f6b94c5aed02b
tp14 <61219940+tp14@users.noreply.github.com>
2022-05-01 20:47:42 -0300

    Delete teg-main directory

--- teg-main/matriz.h
+++
@@ -1,8 +0,0 @@
-#include <stdlib.h>
-#include <stdio.h>
-
-int ** criaMatriz(int * numero_vertices);
-void imprime_matriz(int ** matrix, int n);
-void imprime_numero_nos(int n);
-void imprime_numero_arestas(int ** matrix, int n);
-void imprime_arestas(int ** matrix, int n);
